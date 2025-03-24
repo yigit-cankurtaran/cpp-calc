f67f65a1cbefa9db09af3c39ea17fcecae658702
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-03-24 14:03:43 +0300

    display window + cleanup

--- main.cpp
+++ main.cpp
@@ -22,5 +22,39 @@ int main(int argc, char const *argv[])
         return 1;
     }
 
+    // -1 = first available driver that works
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer)
+    {
+        cout << "renderer creation failed with error: " << SDL_GetError() << endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    bool running = true;
+    SDL_Event event; // anything in the program sdl can detect (key presses mouse clicks etc)
+    while (running)
+    {
+        while (SDL_PollEvent(&event)) // we pass in address so PollEvent can fill it with data
+        {
+            if (event.type == SDL_QUIT)
+            {
+                running = false;
+            }
+        }
+
+        // black screen
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    // cleanup
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
